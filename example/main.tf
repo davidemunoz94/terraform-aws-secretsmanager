c20bc0550c4c715fc0d5b57414bf59f9592f9c61
Madison Hersom <madison.hersom@coalfire.com>
2026-04-28 09:23:23 -0400

    Update main.tf
    change version # to X's

--- example/main.tf
+++ example/main.tf
@@ -3,7 +3,7 @@ data "aws_organizations_organization" "current" {
 }
 
 module "credentials" {
-  source = "git::https://github.com/Coalfire-CF/terraform-aws-secretsmanager?ref=v2.1.7"
+  source = "git::https://github.com/Coalfire-CF/terraform-aws-secretsmanager?ref=vX.X.X"
   providers = {
     aws = aws.mgmt
   }
